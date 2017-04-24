eeb5a757ceab87d44888de8288971180c935dab8
Xuebin He <xuebin.he@emc.com>
2017-04-24 10:56:51 -0400

    Add tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
+var supertest = require('supertest');
 var expect = require('chai').expect;
+
+var server = supertest.agent("http://localhost:3000");
 
 describe('when click the crash button', function() {
   it('should crash website', function(done) {
-
+    server
+      .get('/crash')
+      .end(function(err, res) {
+        expect(err.message).to.contain('ECONNREFUSED: Connection refused');
+        done();
+      });
   });
 });
